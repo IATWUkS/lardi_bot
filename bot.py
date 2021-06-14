bbd42f7315a7cb0a8e8dd50786628c08d25f3e43
IATWUkS <songmaster4@gmail.com>
2021-06-14 12:39:10 +0300

    Изменения интерфейса

--- bot.py
+++ bot.py
@@ -23,30 +23,41 @@ class manage:
 
 @bot.message_handler(commands=['start'])
 def message_handler(message):
-    kb = InlineKeyboardMarkup()
-    cargo_users = InlineKeyboardButton('Грузы партнеров', callback_data='cargo_users')
-    search_cargo = InlineKeyboardButton('Поиск груза', callback_data='search_cargo')
-    kb.add(cargo_users, search_cargo)
     check = db_bot.get_id_tg_check(message.chat.id)
     if check is None or check['ids_tg'] != str(message.chat.id):
         bot.send_message(message.chat.id, 'У вас нет доступа.')
     else:
-        bot.send_message(message.chat.id, 'Выберите функцию:', reply_markup=kb)
+        kb = InlineKeyboardMarkup()
+        start_search = InlineKeyboardButton('Начать мониторинг', callback_data='start_search')
+        stop_search = InlineKeyboardButton('Закончить мониторинг', callback_data='stop_search')
+        add_url_search = InlineKeyboardButton('Добавить ссылку', callback_data='add_url_search')
+        remove_url_search = InlineKeyboardButton('Удалить ссылки', callback_data='remove_url_search')
+        menu = InlineKeyboardButton('Меню', callback_data='menu')
+        kb.add(add_url_search, remove_url_search)
+        status = db_bot.get_id_tg_info(str(message.chat.id))
+        if int(status['status_monit']) == 1:
+            kb.add(start_search)
+            kb.add(menu)
+            bot.send_message(chat_id=message.chat.id, text='Меню '
+                                                           'управлением '
+                                                           'мониторингом ',
+                             reply_markup=kb)
+        if int(status['status_monit']) == 0:
+            kb.add(stop_search)
+            kb.add(menu)
+            bot.send_message(chat_id=message.chat.id, text='Меню '
+                                                           'управлением '
+                                                           'мониторингом\n'
+                                                           'Идёт '
+                                                           'мониторинг.',
+                             reply_markup=kb)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 @async_dec()
 def callback_query(call):
-    if call.data == 'menu':
-        kb = InlineKeyboardMarkup()
-        cargo_users = InlineKeyboardButton('Грузы партнеров', callback_data='cargo_users')
-        search_cargo = InlineKeyboardButton('Поиск груза', callback_data='search_cargo')
-        kb.add(cargo_users, search_cargo)
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                              text='Выберите функцию:',
-                              reply_markup=kb)
     # Поиск груза
-    if call.data == 'search_cargo':
+    if call.data == 'menu':
         kb = InlineKeyboardMarkup()
         start_search = InlineKeyboardButton('Начать мониторинг', callback_data='start_search')
         stop_search = InlineKeyboardButton('Закончить мониторинг', callback_data='stop_search')
@@ -78,7 +89,7 @@ def callback_query(call):
     if call.data == 'remove_url_search':
         urls = db_bot.get_cargo_url_search(str(call.message.chat.id))
         kb = InlineKeyboardMarkup()
-        cargo_users = InlineKeyboardButton('Назад', callback_data='search_cargo')
+        cargo_users = InlineKeyboardButton('Назад', callback_data='menu')
         for url in urls:
             delete_url = InlineKeyboardButton(url['name'], callback_data=str(url['id']) + '_re')
             manage.list_delete_url_next_search.append(str(url['id']) + '_re')
@@ -89,7 +100,7 @@ def callback_query(call):
                               reply_markup=kb)
     if call.data in manage.list_delete_url_next_search:
         kb = InlineKeyboardMarkup()
-        cargo_users = InlineKeyboardButton('Назад', callback_data='search_cargo')
+        cargo_users = InlineKeyboardButton('Назад', callback_data='menu')
         kb.add(cargo_users)
         id_url = call.data.split('_')[0]
         db_bot.delete_cargo_url_search(id_url)
@@ -98,13 +109,13 @@ def callback_query(call):
     if call.data == 'stop_search':
         db_bot.update_status('1', str(call.message.chat.id))
         kb = InlineKeyboardMarkup()
-        cargo_users = InlineKeyboardButton('Назад', callback_data='search_cargo')
+        cargo_users = InlineKeyboardButton('Назад', callback_data='menu')
         kb.add(cargo_users)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                               text='Мониторинг закончен.', reply_markup=kb)
     if call.data == 'add_url_search':
         msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                    text='Введите ссылку(Поиск груза):')
+                                    text='Введите ссылку:')
         bot.register_next_step_handler(msg, add_url_step_search)
     if call.data == 'start_search':
         db_bot.update_status('0', str(call.message.chat.id))
@@ -112,7 +123,7 @@ def callback_query(call):
         status = db_bot.get_id_tg_info(str(call.message.chat.id))
         on_off[str(status['ids_tg'])] = int(status['status_monit'])
         kb = InlineKeyboardMarkup()
-        cargo_users = InlineKeyboardButton('Назад', callback_data='search_cargo')
+        cargo_users = InlineKeyboardButton('Назад', callback_data='menu')
         kb.add(cargo_users)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                               text='Мониторинг начался.',
@@ -122,7 +133,7 @@ def callback_query(call):
             if list_url is None or str(list_url) == '()':
                 kb = InlineKeyboardMarkup()
                 add_url = InlineKeyboardButton('Добавить ссылку', callback_data='add_url_search')
-                back = InlineKeyboardButton('Назад', callback_data='search_cargo')
+                back = InlineKeyboardButton('Назад', callback_data='menu')
                 kb.add(add_url)
                 kb.add(back)
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='Добавте '
@@ -130,10 +141,14 @@ def callback_query(call):
                                                                                                              'ссылку.',
                                       reply_markup=kb)
             if list_url is not None and str(list_url) != '()':
-                list_upload_url = []
+                list_upload_url_gruz = []
+                list_upload_url_truns = []
                 for data in list_url:
-                    list_upload_url.append(data['url'])
-                list_write_all = pars_lardi.get_cargo_search(list_upload_url, str(call.message.chat.id))
+                    if data['url'].split('/')[3] == 'gruz':
+                        list_upload_url_gruz.append(data['url'])
+                    if data['url'].split('/')[3] == 'user':
+                        list_upload_url_truns.append(data['url'])
+                list_write_all = pars_lardi.get_cargo_search(list_upload_url_gruz, str(call.message.chat.id))
                 for url in list_write_all:
                     list_check = db_bot.get_cargo_info_search(url)
                     data_url_info = db_bot.get_cargo_url_search_url(list_check['global_url'])
@@ -143,7 +158,21 @@ def callback_query(call):
                     open_lardi = InlineKeyboardButton('Открыть', url=list_check['url'])
                     manage.list_open_info_search.append(str(list_check['id']))
                     kb.add(open_full_info, open_lardi)
-                    bot.send_message(str(list_check['tg_id']), 'Новая заявка от ' + str(data_url_info['name']), reply_markup=kb)
+                    bot.send_message(str(list_check['tg_id']), 'Новая заявка от ' + str(data_url_info['name']),
+                                     reply_markup=kb)
+                list_write_all = pars_lardi.get_cargo(list_upload_url_truns, str(call.message.chat.id))
+                for URL in list_write_all:
+                    kb = InlineKeyboardMarkup()
+                    list_check = db_bot.get_cargo_info(URL)
+                    open_full_info = InlineKeyboardButton('Подробнее',
+                                                          callback_data=str(list_check['id']))
+                    open_lardi = InlineKeyboardButton('Открыть', url=list_check['url'])
+                    manage.list_open_info.append(str(list_check['id']))
+                    kb.add(open_full_info, open_lardi)
+                    try:
+                        bot.send_message(list_check['tg_id'], list_check['name'], reply_markup=kb)
+                    except:
+                        pass
             print('круг')
             time.sleep(60)
             status = db_bot.get_id_tg_info(str(call.message.chat.id))
@@ -344,7 +373,7 @@ def add_url_step_search(message):
     try:
         if URL == '/start':
             kb = InlineKeyboardMarkup()
-            cargo_users = InlineKeyboardButton('Назад', callback_data='search_cargo')
+            cargo_users = InlineKeyboardButton('Назад', callback_data='menu')
             kb.add(cargo_users)
             bot.send_message(message.chat.id, 'Отмена ввода, нажмите кнопку', reply_markup=kb)
         else:
@@ -359,18 +388,17 @@ def add_url_step_search_2(message, URL):
     try:
         if name == '/start':
             kb = InlineKeyboardMarkup()
-            cargo_users = InlineKeyboardButton('Назад', callback_data='search_cargo')
+            cargo_users = InlineKeyboardButton('Назад', callback_data='menu')
             kb.add(cargo_users)
             bot.send_message(message.chat.id, 'Отмена ввода, нажмите кнопку', reply_markup=kb)
         else:
             db_bot.insert_cargo_url_search(URL, str(message.chat.id), name)
             kb = InlineKeyboardMarkup()
-            cargo_users = InlineKeyboardButton('Назад', callback_data='search_cargo')
+            cargo_users = InlineKeyboardButton('Назад', callback_data='menu')
             kb.add(cargo_users)
             bot.send_message(message.chat.id, 'URL добавлен.', reply_markup=kb)
     except:
         bot.send_message(message.chat.id, 'Ошибка добавления URL')
-
 
 
 bot.polling()
--- pars_lardi.py
+++ pars_lardi.py
@@ -11,20 +11,44 @@ LOGIN = data.LOGIN
 PASSWORD = data.PASSWORD
 
 
+class data:
+    cockies = None
+
+
+def get_cockies():
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        browser = webdriver.Chrome(options=options)
+        browser.get('https://lardi-trans.com/ru/accounts/login/')
+        browser.find_element_by_css_selector('#loginData').send_keys(LOGIN)
+        browser.find_element_by_css_selector('#password').send_keys(
+            PASSWORD)
+        time.sleep(3)
+        browser.find_element_by_css_selector('#loginButton').click()
+        time.sleep(10)
+        cockie = browser.get_cookies()
+        cockies = cockie[0]['value'] + ';' + cockie[1]['name'] + '=' + cockie[1]['value'] + ';' + cockie[2][
+            'name'] + '=' + \
+                  cockie[2]['value'] + ';' + cockie[3]['name'] + '=' + cockie[3]['value'] + ';' + cockie[4][
+                      'name'] + '=' + \
+                  cockie[4]['value'] + ';' + cockie[5]['name'] + '=' + cockie[5]['value'] + ';' + cockie[6][
+                      'name'] + '=' + \
+                  cockie[6]['value'] + ';' + cockie[7]['name'] + '=' + cockie[7]['value'] + ';' + cockie[8][
+                      'name'] + '=' + \
+                  cockie[8]['value'] + ';' + cockie[9]['name'] + '=' + cockie[9]['value'] + ';' + cockie[10][
+                      'name'] + '=' + \
+                  cockie[10]['value'] + ';' + cockie[11]['name'] + '=' + cockie[11]['value'] + ';' + cockie[12][
+                      'name'] + '=' + cockie[12]['value'] + ';' + cockie[13]['name'] + '=' + cockie[13]['value'] + ';' + \
+                  cockie[14]['name'] + '=' + cockie[14]['value'] + ';' + cockie[15]['name'] + '=' + cockie[15][
+                      'value'] + ';' + cockie[16]['name'] + '=' + cockie[16]['value'] + ';'
+        data.cockies = cockies
+        browser.close()
+
+
 def get_html(URL):
-    options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
-    browser = webdriver.Chrome(options=options)
-    browser.get('https://lardi-trans.com/ru/accounts/login/')
-    browser.find_element_by_css_selector('#loginData').send_keys(LOGIN)
-    browser.find_element_by_css_selector('#password').send_keys(
-        PASSWORD)
-    browser.find_element_by_css_selector('#loginButton').click()
-    time.sleep(10)
-    cockie = browser.get_cookies()
-    cockies = cockie[0]['value'] + ';' + cockie[1]['name'] + '=' + cockie[1]['value'] + ';' + cockie[2]['name'] + '=' + cockie[2]['value'] + ';' + cockie[3]['name'] + '=' + cockie[3]['value'] + ';' + cockie[4]['name'] + '=' + cockie[4]['value'] + ';' + cockie[5]['name'] + '=' + cockie[5]['value'] + ';' + cockie[6]['name'] + '=' + cockie[6]['value'] + ';' + cockie[7]['name'] + '=' + cockie[7]['value'] + ';' + cockie[8]['name'] + '=' + cockie[8]['value'] + ';' + cockie[9]['name'] + '=' + cockie[9]['value'] + ';' + cockie[10]['name'] + '=' + cockie[10]['value'] + ';' + cockie[11]['name'] + '=' + cockie[11]['value'] + ';' + cockie[12]['name'] + '=' + cockie[12]['value'] + ';' + cockie[13]['name'] + '=' + cockie[13]['value'] + ';' + cockie[14]['name'] + '=' + cockie[14]['value'] + ';' + cockie[15]['name'] + '=' + cockie[15]['value'] + ';' + cockie[16]['name'] + '=' + cockie[16]['value'] + ';'
+    get_cockies()
     HEADERS = {
-        'cookie': cockies,
+        'cookie': data.cockies,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
         'Connection': 'keep-alive',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
@@ -66,27 +90,27 @@ def get_cargo(URL_list, tg_id):
 def get_cargo_search(URL_list, tg_id):
     all_list_write = []
     for URL in URL_list:
-            page = get_html(URL)
-            soup = BeautifulSoup(page, "html.parser")
-            cargo = soup.find('div', class_='ps_search-result_data')
-            all_cargo_info = cargo.findAll('div', class_='ps_search-result_data-item')
-            for inf in all_cargo_info:
-                url = 'https://lardi-trans.com/gruz/view/' + str(inf).split('data-ps-id="')[1].split('"')[0]
-                country = inf.find('span', class_='ps_data_direction').text.replace('\n', '')
-                date = inf.find('span', class_='ps_data_load-date').text.replace('\n', '')
-                transport = inf.find('span', class_='ps_data_transport').text.replace('\n', '')
-                from_is = inf.find('span', class_='ps_data_town').text.replace('\n', '')
-                to_is = inf.find('span', class_='ps_data_town').text.replace('\n', '')
-                cargo = inf.find('div', class_='ps_data ps_search-result_data-cargo ps_data-cargo').text.replace('\n',
-                                                                                                                 '')
-                pay = inf.find('span', class_='ps_data_payment_info').text.replace('\n', '')
-                contacts = inf.find('div', class_='ps_proposal_user_name-container').text.replace('\n', '')
-                check = db_bot.get_cargo_search(url)
-                if check is None or check['url'] != url:
-                    try:
-                        db_bot.insert_carge_info_search(country, date, transport, from_is, to_is, cargo, pay, contacts,
-                                                        url, tg_id, URL)
-                        all_list_write.append(url)
-                    except:
-                        pass
+        page = get_html(URL)
+        soup = BeautifulSoup(page, "html.parser")
+        cargo = soup.find('div', class_='ps_search-result_data')
+        all_cargo_info = cargo.findAll('div', class_='ps_search-result_data-item')
+        for inf in all_cargo_info:
+            url = 'https://lardi-trans.com/gruz/view/' + str(inf).split('data-ps-id="')[1].split('"')[0]
+            country = inf.find('span', class_='ps_data_direction').text.replace('\n', '')
+            date = inf.find('span', class_='ps_data_load-date').text.replace('\n', '')
+            transport = inf.find('span', class_='ps_data_transport').text.replace('\n', '')
+            from_is = inf.find('span', class_='ps_data_town').text.replace('\n', '')
+            to_is = inf.find('span', class_='ps_data_town').text.replace('\n', '')
+            cargo = inf.find('div', class_='ps_data ps_search-result_data-cargo ps_data-cargo').text.replace('\n',
+                                                                                                             '')
+            pay = inf.find('span', class_='ps_data_payment_info').text.replace('\n', '')
+            contacts = inf.find('div', class_='ps_proposal_user_name-container').text.replace('\n', '')
+            check = db_bot.get_cargo_search(url)
+            if check is None or check['url'] != url:
+                try:
+                    db_bot.insert_carge_info_search(country, date, transport, from_is, to_is, cargo, pay, contacts,
+                                                    url, tg_id, URL)
+                    all_list_write.append(url)
+                except:
+                    pass
     return all_list_write
